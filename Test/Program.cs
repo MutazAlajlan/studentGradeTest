7f3dbe27798b481a0e04f554022c41743fcef01d
Mutaz Alajlan <Mutaz.Alajlan@tuwaiq.edu.sa>
2021-05-08 20:07:03 +0100

    Another version which was stopped.

---
+++ Test/Program.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+namespace Test
+{
+    class Program
+    {
+        static bool IsNumeric(String str)
+        {
+            bool result = int.TryParse(str, out _);
+            return result;
+        }
+
+        static int GetMark(String str)
+        {
+            int mark;
+            int.TryParse(str, out mark);
+            return mark;
+        }
+
+        static bool IsCharacters(String str)
+        {
+            bool result = Regex.IsMatch(str, @"^[a-zA-Z]+$");
+
+            return result;
+        }
+
+        static bool MarkInRange (int mark)
+        {
+            
+            if (mark <= 100 && mark >= 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        static bool IsQuit(string str)
+        {
+            return str.Equals("QUIT", StringComparison.OrdinalIgnoreCase);
+        }
+
+        static void Main(string[] args)
+        {
+            /* takes names array.
+               takes marks array.
+               arrays are not fixed length.
+            at the end of insertion, display all the names and marks.
+            Print Fail if mark is LESS than 60.
+            Print Passed if the mark is 60 or more.
+             */
+            
+            //Console.WriteLine("Hello World!");
+
+            bool contBool = true;
+            Student students = new Student();
+
+            Console.WriteLine("Hello and welcome to the grading system!");
+            while (contBool)
+            {
+                
+                Console.WriteLine("Please enter the student name or write QUIT to exit the program:");
+                String name = Console.ReadLine();
+                if (IsQuit(name))
+                {
+                    students.Print();
+                }
+                if (!IsNumeric(name))
+                {
+                    if (IsCharacters(name))
+                    {
+                        //add
+                        students.AddName(name);
+                        //Add marks
+                        Console.WriteLine("Please enter the student mark:");
+                        String stringMark = Console.ReadLine();
+                        while (!IsNumeric(stringMark))
+                        {
+                            Console.WriteLine("Please enter only digits.");
+                            stringMark = Console.ReadLine();
+                        }
+                        if(IsNumeric(stringMark))
+                        {
+                            int intMark = GetMark(stringMark);
+                            if(MarkInRange(intMark))
+                            {
+                                //add
+                                students.AddMark(intMark);
+                                Console.WriteLine("The student {0}, with a mark of {1} has been added.", name, intMark);
+                            }
+                            else
+                            {
+                                Console.WriteLine("Must be from 0 to 100");
+                            }
+
+                        }
+                        else
+                        {
+                            Console.WriteLine("There has been an error in entering the marks. It should be digits only.");
+                        }
+
+                        
+                    }
+                    else
+                    {
+                        Console.WriteLine("Please enter only characters in the name field.");
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Please only use characters. No digits are allowed");
+                }
+
+            }
+
+        }
+    }
+     class Student
+    {
+         List<string> studentsName = new List<string>();
+         List<int> studentsMark = new List<int>();
+
+         public void Print()
+        {
+            //foreach 
+            //Console.WriteLine()
+            int index = studentsMark.Count-1;
+            for (int i = 0; i <= index; i++)
+            {
+                string msg = "==========================";
+                msg += "Student: " + studentsName[i];
+                msg += "  |  His mark is: " + studentsMark[i];
+                msg += "||  ";
+                if (studentsMark[i] >= 60)
+                {
+                    msg += "Passed";
+                }
+                else if (studentsMark[i] < 60)
+                {
+                    msg += "Fail";
+                }
+                Console.WriteLine(msg + "\n ==========================");
+            }
+
+        }
+        //function to add to names list
+         public void AddName(String str)
+        {
+             studentsName.Add(str);
+        }
+        public void AddMark(int mark)
+        {
+            studentsMark.Add(mark);
+        }
+        public string GetName(int index)
+        {
+            if(studentsName.Count >= index)
+            {
+                return studentsName[index];
+            }
+            else
+            {
+                return "There has been an error accessing the student name.";
+            }
+        }
+        public int GetMark(int index)
+        {
+            if(studentsMark.Count >= index)
+            {
+                return studentsMark[index];
+            }
+            else
+            {
+                throw new ArgumentException("There has been an error in getting marks list.");
+            }
+        }
+        public Student()
+        {
+            studentsName = new List<string>();
+            studentsMark = new List<int>();
+        }
+    }
+}
---
+++ Test/Test.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+namespace Test
+{
+    class Program2
+    {
+        class Student
+        {
+            static int id = 1;
+            string name;
+            int mark;
+            string result;
+
+            bool IsCharacters(string name)
+            {
+                return Regex.IsMatch(name, @"^[a-zA-Z]+$");
+            }
+            bool ValidateName(string name)
+            {
+                return IsCharacters(name);
+            }
+            bool IsNumeric(int mark)
+            {
+                return double.TryParse(mark, out _);
+            }
+            /*
+            SetName(string name)
+            {
+                if(ValidateName(name))
+                {
+                    return name;
+                }
+            }
+            */
+            bool MarkInRange(int mark)
+            {
+                if (mark >= 0 && mark <= 100)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            bool ValidateMark(int mark)
+            {
+                if (IsNumeric(mark))
+                {
+                    if (MarkInRange(mark))
+                    {
+                        return true;
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            string MarkResult(int mark)
+            {
+                if (mark >= 60)
+                {
+                    return "Passed";
+                }
+                else if (mark < 60)
+                {
+                    return "Fail";
+                }
+            }
+            string ToString(string name, int mark, string result)
+            {
+                return "Student: " + name + " Mark: " + mark + " | " + result;
+            }
+
+            public static Student Create(string name, string stringMark)
+            {
+                if (ValidateName(name))
+                {
+                    if (ValidateMark(stringMark))
+                    {
+                        string result = MarkResult(mark);
+                        //return Student Student = new Student;
+                        id++;
+                    }
+                    else
+                    {
+                        return "mark";
+                    }
+                }
+                else
+                {
+                    return "name";
+                }
+            }
+            private Student(int studentId, string studentName, int studentMark, string grade)
+            {
+                //what if it fail? will still increment
+
+                //validate first before insertion:
+                this.id = studentId;
+                this.name = studentName;
+                this.mark = studentMark;
+                this.result = grade;
+            }
+        }
+        class Students
+        {
+            List<Student> studentsList;
+
+            public AddStudent(Student studentObject)
+            {
+                this.Add(studentObject);
+            }
+            public print()
+            {
+                Console.WriteLine("The result of each student is: ");
+                foreach (int index in studentsList)
+                {
+                    msg = studentsList[index].ToString();
+                    Console.WriteLine("==========================\n" + msg);
+                }
+            }
+            public bool IsEmpty()
+            {
+                if (this.studentsList.Any())
+                {
+                    return false;
+                }
+                else
+                {
+                    return true;
+                }
+            }
+            public Students()
+            {
+                studentsList = new List<Student>();
+            }
+        }
+        public int ConvertToInt(string mark)
+        {
+            int intMark;
+            if (int.TryParse(mark, out intMark))
+            {
+                return intMark;
+            }
+            else
+            {
+                return -1;
+            }
+        }
+        public bool IsQuit(string input)
+        {
+            if (input.Equals("QUIT", StringComparison.OrdinalIgnoreCase))
+            {
+                /*
+                bool ListNotEmpty = studentsList.Any();
+                if (studentsList.IsEmpty())
+                {
+                    Environment.Exit(0);
+                }
+                else if (!studentsList.IsEmpty())
+                {
+                    studentsList.Print();
+                    Environment.Exit(0);
+                }
+                */
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public Quit(Students students)
+        {
+            bool ListNotEmpty = students.Any();
+            if (students.IsEmpty())
+            {
+                Environment.Exit(0);
+            }
+            else if (!students.IsEmpty())
+            {
+                students.Print();
+                Environment.Exit(0);
+            }
+        }
+        public static void Main(String[] args)
+        {
+            Console.Write(
+@"   _____               _ _                _____           _                 
+  / ____|             | (_)              / ____|         | |                
+ | |  __ _ __ __ _  __| |_ _ __   __ _  | (___  _   _ ___| |_ ___ _ __ ___  
+ | | |_ | '__/ _` |/ _` | | '_ \ / _` |  \___ \| | | / __| __/ _ \ '_ ` _ \ 
+ | |__| | | | (_| | (_| | | | | | (_| |  ____) | |_| \__ \ ||  __/ | | | | |
+  \_____|_|  \__,_|\__,_|_|_| |_|\__, | |_____/ \__, |___/\__\___|_| |_| |_|
+                                  __/ |          __/ |                      
+                                 |___/          |___/                       
+");
+            Console.WriteLine("Please enter the student name or write QUIT to exit the program");
+            string userName = Console.ReadLine();
+            Students studentsList = new Students();
+            if (isQuit)
+            {
+                quit(studentsList);
+            }
+            else
+            {
+                string stringMark = Console.ReadLine();
+                userMark = ConvertToInt(stringMark);
+            }
+            string stringMark = Console.ReadLine();
+            userMark = ConvertToInt(stringMark);
+
+        }
+    }
+}
